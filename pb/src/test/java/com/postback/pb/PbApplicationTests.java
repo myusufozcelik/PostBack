d2fe360765dfd90f4f4a8b4b20e8cac6e0087e1d
myusuf <myusufozcelik96@gmail.com>
2021-08-08 22:29:32 +0300

    project created

---
+++ pb/src/test/java/com/postback/pb/PbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postback.pb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

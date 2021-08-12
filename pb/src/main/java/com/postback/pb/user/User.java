74dcf2613c022fb77fd2633e5eabc0373136d614
myusuf <myusufozcelik96@gmail.com>
2021-08-12 21:49:37 +0300

    findByUsername  and some control added

--- pb/src/main/java/com/postback/pb/user/User.java
+++ pb/src/main/java/com/postback/pb/user/User.java
@@ -1,9 +1,12 @@
 package com.postback.pb.user;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -15,10 +18,18 @@ public class User {
 	@GeneratedValue
 	private long id;
 
+	@NotNull
+	@Size(min = 4, max = 255)
+	@UniqueUsername
 	private String username;
 
+	@NotNull
+	@Size(min = 4, max = 255)
 	private String displayName;
 
+	@NotNull
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
+	@Size(min = 8, max = 255)
 	private String password;
 
 }
--- pb/src/main/java/com/postback/pb/user/UserController.java
+++ pb/src/main/java/com/postback/pb/user/UserController.java
@@ -3,12 +3,15 @@ package com.postback.pb.user;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -26,31 +29,22 @@ public class UserController {
 	UserService userService;
 
 	@PostMapping("/api/v1/users")
-	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<?> createUser(@RequestBody User user) {
+	public GenericResponse createUser(@Valid @RequestBody User user) {
+		userService.save(user);
+		return new GenericResponse("User Created", HttpStatus.CREATED.value());
+	}
+
+// MethodArgumentNotValidException atılırsa aşağıdaki metodu çalıştır
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ApiError handleValidationException(MethodArgumentNotValidException exception) {
 		ApiError error = new ApiError(400, "Validation Error", "/api/v1/users");
 		Map<String, String> validationErrors = new HashMap<String, String>();
-		
-		String username = user.getUsername();
-		String displayName = user.getDisplayName();
-		
-		if (username == null || username.isEmpty()) {
-			validationErrors.put("username", "Username cannot be null");
+		for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
 		}
-		
-		if (displayName == null || displayName.isEmpty()) {
-			validationErrors.put("displayName", "Display Name Cannot be null");
-		}
-		
-		if (validationErrors.size() > 0 ) {
-			error.setValidationErrors(validationErrors);
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
-		}
-		
-		userService.save(user);
-		return ResponseEntity.ok(new GenericResponse("User Created", HttpStatus.CREATED.value()));
+		error.setValidationErrors(validationErrors);
+		return error;
 	}
-	
-
 
 }
